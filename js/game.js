71238b356af43897a078a73cdc44f7745ddd81aa
TomiokaGiyuuu <97001425+TomiokaGiyuuu@users.noreply.github.com>
2022-02-22 15:29:04 +0600

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
-import { GAME } from "js/vars.js";
-import { Profile, isDraw, endGame, setHoverEffect, markCell, swapTurns } from "js/tic.js";
-import { checkWin, WIN_COMBINATIONS } from 'js/win.js';
+import { GAME } from "vars.js";
+import { Profile, isDraw, endGame, setHoverEffect, markCell, swapTurns } from "tic.js";
+import { checkWin, WIN_COMBINATIONS } from 'win.js';
 
 
 GAME.startBtn.addEventListener("click", startGame);
